6b8139ab8b3b3b2620cb3e310c874f3f87591543
Gabriel D. Amaral <gabrieldutraamaral@gmail.com>
2025-10-22 02:28:11 -0300

    Parse API Version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,9 +18,19 @@ public class Main {
        byte[] apiKey = in.readNBytes(2);
        byte[] apiVersion = in.readNBytes(2);
        byte[] correlationId = in.readNBytes(4);
+       byte[] error_code = new byte[2];
+       short errorValue = 35;
+
+       short apiVersionDecimal = (short) (((apiVersion[0] & 0xFF) << 8) | (apiVersion[1] & 0xFF));
+
+       if(apiVersionDecimal < 0 || apiVersionDecimal > 4) {
+           error_code[0] = (byte) (errorValue >> 8);
+           error_code[1] = (byte) errorValue;
+       }
 
        clientSocket.getOutputStream().write(messageSizeBytes);
        clientSocket.getOutputStream().write(correlationId);
+       clientSocket.getOutputStream().write(error_code);
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      } finally {
